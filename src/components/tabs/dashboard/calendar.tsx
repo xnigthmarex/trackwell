dc1347d98815e87a1732525e923218e3616b50c6
Anish <ani.bellamkonda@gmail.com>
2024-03-05 23:12:35 -0600

    new dependencies, calendar using MUI added

---
+++ src/components/tabs/dashboard/calendar.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { makeStyles } from '@mui/material';
+
+
+export default function BasicDateCalendar() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar 
+      className='color: red,
+      color: white'/>
+    </LocalizationProvider>
+  );
+}
