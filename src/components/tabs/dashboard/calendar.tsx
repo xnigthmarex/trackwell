1d1c47ec364f53046cd987aa9ef3365973886645
Anish <ani.bellamkonda@gmail.com>
2024-03-20 22:35:46 -0500

    why is the documentation so bad?

--- src/components/tabs/dashboard/calendar.tsx
+++ src/components/tabs/dashboard/calendar.tsx
@@ -1,14 +1,19 @@
-import * as React from 'react';
 
+import { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
+type ValuePiece = Date | null;
 
-
-
-
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function BasicDateCalendar() {
+  const [value, onChange] = useState<Value>(new Date());
+
   return (
-    <Calendar className={'ml-4'}/>
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
   );
 }
+
